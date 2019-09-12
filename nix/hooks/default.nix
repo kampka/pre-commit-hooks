00871f6c5d2f7a0b85089e79c78f37aa3d7ca024
Christian Kampka <christian@kampka.net>
2019-09-12 21:34:19 +0200

    Remove skip script support

--- nix/hooks/default.nix
+++ nix/hooks/default.nix
@@ -5,27 +5,17 @@ let
   mkHook = {
     name,
     PATH ? [],
-    skipScript ? null,
     hookScript
   } : let 
 
     path = pkgs.writeShellScriptBin name ''
 
-    set -u
+    set -eu
     set -o pipefail
     
     ${lib.optionalString (PATH != []) ''export PATH=$PATH:${ lib.makeBinPath PATH } ''  }
 
-    ${lib.optionalString (skipScript != null) ''
-      if ! "${pkgs.writeShellScriptBin "${name}-skip" skipScript}/bin/${name}-skip"; then
-        exit 123
-      fi
-    ''}
-  
-    if ! "${pkgs.writeShellScriptBin "${name}-hook" hookScript}/bin/${name}-hook"; then
-      exit 1
-    fi
-    exit 0
+    exec "${pkgs.writeShellScriptBin "${name}-hook" hookScript}/bin/${name}-hook"
   '';
   in {
     name = name;
